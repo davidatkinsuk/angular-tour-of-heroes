8f777fb75ba31550c1ec8d39bb255af409d63dd2
davidatkinsuk <david.atkins@atos.net>
2018-09-11 07:26:14 +0100

    completed section 6

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -15,6 +15,7 @@ export class HeroesComponent implements OnInit {
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    // initialise the heroes list
     this.getHeroes();
   }
 
